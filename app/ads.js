5b1b25e1877fd89278b0c524eb742a1ba849047b
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2023-01-27 12:43:17 +0200

    Update ads.js

--- app/ads.js
+++ app/ads.js
@@ -50,10 +50,7 @@ function setUpIMA() {
 
   // Request video ads.
   const adsRequest = new google.ima.AdsRequest();
-  adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
-      'iu=/21775744923/external/single_ad_samples&sz=640x480&' +
-      'cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&' +
-      'output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=';
+  adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?ad_type=audio_video&sz=200x200&iu=/22826216928/1234555&ciu_szs='fluid'&env=instream&impl=s&gdfp_req=1&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]"';
 
   // Specify the linear and nonlinear slot sizes. This helps the SDK to
   // select the correct creative if multiple are returned.
